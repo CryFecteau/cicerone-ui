14f651068f07b53ce29532a5fa5dd32c8323e497
CryFecteau <Crystal_Fecteau@ymail.com>
2020-08-17 10:16:06 -0700

    Updates

---
+++ precache-manifest.fe1f73f5851e3463b7eea46497b4c707.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed977f1ae87f1d9dfef7884d6a59f3d2",
+    "url": "/cicerone-ui/index.html"
+  },
+  {
+    "revision": "6435bde06e1517e9a043",
+    "url": "/cicerone-ui/static/css/2.b4e040ed.chunk.css"
+  },
+  {
+    "revision": "45ef797d87eb2563ae85",
+    "url": "/cicerone-ui/static/css/main.f9a50ced.chunk.css"
+  },
+  {
+    "revision": "6435bde06e1517e9a043",
+    "url": "/cicerone-ui/static/js/2.513391e2.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/cicerone-ui/static/js/2.513391e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45ef797d87eb2563ae85",
+    "url": "/cicerone-ui/static/js/main.c8cdd537.chunk.js"
+  },
+  {
+    "revision": "bd2f3afba48cda1988e4",
+    "url": "/cicerone-ui/static/js/runtime-main.1822d019.js"
+  },
+  {
+    "revision": "c4cbb56677c5a896191572481da4eeb4",
+    "url": "/cicerone-ui/static/media/barley.c4cbb566.jpg"
+  },
+  {
+    "revision": "6dfb274ca037e298bffab90cef0cc755",
+    "url": "/cicerone-ui/static/media/beer.6dfb274c.jpg"
+  },
+  {
+    "revision": "85ebe3c10cb5094a2f316aa86007b977",
+    "url": "/cicerone-ui/static/media/beerGlass.85ebe3c1.jpg"
+  },
+  {
+    "revision": "855d6ee970576624edf8bc565c09b858",
+    "url": "/cicerone-ui/static/media/beerOpener.855d6ee9.jpg"
+  },
+  {
+    "revision": "64771251444eee3fe8e3b0eac626a6c8",
+    "url": "/cicerone-ui/static/media/femaleServer.64771251.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cicerone-ui/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cicerone-ui/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cicerone-ui/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cicerone-ui/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cicerone-ui/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "bfeaa126013b133a5f6098c976e96625",
+    "url": "/cicerone-ui/static/media/hops.bfeaa126.jpg"
+  },
+  {
+    "revision": "43dc64efaa86e5da3c831920d708ab6d",
+    "url": "/cicerone-ui/static/media/pairing.43dc64ef.jpg"
+  },
+  {
+    "revision": "a157c5bc545a5a804d43dd69269f4869",
+    "url": "/cicerone-ui/static/media/pouring.a157c5bc.jpg"
+  },
+  {
+    "revision": "1f70c11e5df2a44f7898d9231d7278ba",
+    "url": "/cicerone-ui/static/media/servingBeer.1f70c11e.jpg"
+  },
+  {
+    "revision": "8f226c42fd9cbe3a4abd150a26008449",
+    "url": "/cicerone-ui/static/media/taps.8f226c42.jpg"
+  },
+  {
+    "revision": "f2431518aadd2f7274cc60c80362272f",
+    "url": "/cicerone-ui/static/media/tasting.f2431518.jpg"
+  },
+  {
+    "revision": "0ff45e9bef5523132435255b057747a8",
+    "url": "/cicerone-ui/static/media/tastingTable.0ff45e9b.jpg"
+  }
+]);
