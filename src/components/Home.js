cf33b277dea9b0d405c82c5e01098574c7df2fa6
CryFecteau <Crystal_Fecteau@ymail.com>
2020-08-19 11:30:34 -0700

    fix footer links, add login modal, adjust study material style

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,38 +24,38 @@ function Home() {
                 <CardGroup className='container mt-3'>
                     <Card>
                         <CardImg top width='100%' src={beerOpener} alt='opening beer' />
-                        <CardBody>
+                        <CardBody className='d-flex flex-column'>
                             <CardTitle className='text-center'>Certified Beer Server</CardTitle>
                             <CardSubtitle>Level 1</CardSubtitle>
                             <CardText >Great beer service begins with the fundamentals: beer clean glassware, a proper pour, and beer that hasn’t been ruined by improper handling.</CardText>
-                            <Button outline color='warning'><Link to='beer-server'>Learn More</Link></Button>
+                            <Button className='mt-auto' outline color='warning'><Link to='beer-server'>Learn More</Link></Button>
                         </CardBody>
                     </Card>
                     <Card>
                         <CardImg top width='100%' src={servingBeer} alt='serving beer' />
-                        <CardBody>
+                        <CardBody className='d-flex flex-column'>
                             <CardTitle className='text-center'>Certified Cicerone</CardTitle>
                             <CardSubtitle>Level 2</CardSubtitle>
                             <CardText>Those who pursue a career in beer learn from many different sources and soon accumulate knowledge that sets them apart from those on the 'customer' side of the bar.</CardText>
-                            <Button outline color='warning'><Link to='cicerone'>Learn More</Link></Button>
+                            <Button className='mt-auto' outline color='warning'><Link to='cicerone'>Learn More</Link></Button>
                         </CardBody>
                     </Card>
                     <Card>
                         <CardImg top width='100%' src={tasting} alt='beer tasting' />
-                        <CardBody>
+                        <CardBody className='d-flex flex-column'>
                             <CardTitle className='text-center'>Advanced Cicerone</CardTitle>
                             <CardSubtitle>Level 3</CardSubtitle>
                             <CardText>The 3rd-level rank of Advanced Cicerone was introduced in 2015 to distinguish those individuals who had advanced their knowledge to an expert level. </CardText>
-                            <Button outline color='warning'><Link to='advanced-cicerone'>Learn More</Link></Button>
+                            <Button className='mt-auto' outline color='warning'><Link to='advanced-cicerone'>Learn More</Link></Button>
                         </CardBody>
                     </Card>
                     <Card>
                         <CardImg top width='100%' src={pairing} alt='pairing beer' />
-                        <CardBody>
+                        <CardBody className='d-flex flex-column'>
                             <CardTitle className='text-center'>Master Cicerone</CardTitle>
                             <CardSubtitle>Level 4</CardSubtitle>
                             <CardText>The Master Cicerone exam challenges individuals to master every technical and aesthetic aspect of beer.</CardText>
-                            <Button outline color='warning'><Link to='master-cicerone'>Learn More</Link></Button>
+                            <Button className='mt-auto' outline color='warning'><Link to='master-cicerone'>Learn More</Link></Button>
                         </CardBody>
                     </Card>
                 </CardGroup>
--- src/components/StudyMaterial.js
+++ src/components/StudyMaterial.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Tabs, Tab, TabList, TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.css';
 import { Card, Button, CardImg, CardTitle, CardText, CardGroup, CardSubtitle, CardBody, Progress } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import bottle from '../assets/images/beerOpener.jpg';
@@ -17,154 +18,211 @@ import barley from '../assets/images/barley.jpg';
 export default function StudyMaterial() {
     return (
         <div className='container'>
-            <h3 className='header-line mt-4'>
-                Study Material
+            <div className='text-center mt-4'>
+                <h3 className='header-line mt-4'>
+                    Study Material
             </h3>
-            <h6>Select Level:</h6>
-            <Tabs>
+            </div>
+            <Tabs className='mt-4' defaultIndex={0}>
                 <TabList>
                     <Tab>Beer Server</Tab>
                     <Tab>Cicerone</Tab>
                     <Tab>Advanced Cicerone</Tab>
                     <Tab>Master Cicerone</Tab>
                 </TabList>
-
                 <TabPanel>
-                    <h3>Beer Server</h3>
-                    <p>Level 1</p>
+                    <div className='text-center mt-4'>
+                        <h3>Beer Server</h3>
+                        <p>Level 1</p>
+                    </div>
+                    <div>
+                        <h5>Overview:</h5>
+                        <ul>
+                            <li>Become conversant in the language of beer, including key flavor words and core beer styles</li>
+                            <li>Master the details of beer service, including glass preparation and proper pouring of both draft and bottled beers</li>
+                            <li>Learn the three most common ways the flavor of beer can be ruined after it leaves the brewery and what must be done to prevent this from happening</li>
+                        </ul>
+                        <h5>Prerequisites</h5>
+                        <ul>
+                            <li>Passion for beer</li>
+                            <li>Legal drinking age</li>
+                        </ul>
+                    </div>
                     <div className='text-center'>10% Completed</div>
                     <Progress animated striped color='info' value={10} />
                     <CardGroup className='container  mb-5'>
                         <Card className='m-2'>
-                            <CardImg top width='100%' src={bottle} alt='beer bottles' />
-                            <CardBody>
-                                <CardTitle className='text-center'>Beer Server Quiz</CardTitle>
+                            <CardImg top width='100%' src={femaleServer} alt='female beer server' />
+                            <CardBody className='d-flex flex-column'>
+                                <CardTitle className='text-center'>Beer Server Manual</CardTitle>
                                 <CardSubtitle>Level 1</CardSubtitle>
-                                <CardText>Test your knowledge and take the Beer Server Quiz</CardText>
-                                <Button outline color='info'><Link to='content'>Start</Link></Button>
+                                <CardText>Gain the knowledge to pass the Beer Server Exam</CardText>
+                                <Button className='mt-auto' outline color='info'><Link to='content'>Study</Link></Button>
                             </CardBody>
                         </Card>
                         <Card className='m-2'>
                             <CardImg top width='100%' src={servingBeer} alt='serving beer' />
-                            <CardBody>
+                            <CardBody className='d-flex flex-column'>
                                 <CardTitle className='text-center'>Beer Server Flashcards</CardTitle>
                                 <CardSubtitle>Level 1</CardSubtitle>
                                 <CardText>Prep and Review with Beer Server Flashcards</CardText>
-                                <Button outline color='info'><Link to='content'>Review</Link></Button>
+                                <Button className='mt-auto' outline color='info'><Link to='content'>Review</Link></Button>
                             </CardBody>
                         </Card>
                         <Card className='m-2'>
-                            <CardImg top width='100%' src={femaleServer} alt='female beer server' />
-                            <CardBody>
-                                <CardTitle className='text-center'>Beer Server Manual</CardTitle>
+                            <CardImg top width='100%' src={bottle} alt='beer bottles' />
+                            <CardBody className='d-flex flex-column'>
+                                <CardTitle className='text-center'>Beer Server Quiz</CardTitle>
                                 <CardSubtitle>Level 1</CardSubtitle>
-                                <CardText>Gain the knowledge to pass the Beer Server Exam</CardText>
-                                <Button outline color='info'><Link to='content'>Study</Link></Button>
+                                <CardText>Test your knowledge and take the Beer Server Quiz</CardText>
+                                <Button className='mt-auto' outline color='info'><Link to='content'>Start</Link></Button>
                             </CardBody>
                         </Card>
                     </CardGroup>
                 </TabPanel>
                 <TabPanel>
-                    <h3>Cicerone</h3>
-                    <p>Level 2</p>
+                    <div className='text-center mt-4'>
+                        <h3>Cicerone</h3>
+                        <p>Level 2</p>
+                    </div>
+                    <div>
+                        <h5>Overview:</h5>
+                        <ul>
+                            <li>Those who achieve this certification have a solid and well-rounded knowledge of beer and beer service as well as basic competence in assessing beer quality and identity by taste.</li>
+                        </ul>
+                        <h5>Prerequisites</h5>
+                        <ul>
+                            <li>Passed the Certified Beer Server Exam</li>
+                            <li>Legal drinking age</li>
+                        </ul>
+                    </div>
                     <div className='text-center'>5% Completed</div>
                     <Progress animated striped color='info' value={5} />
                     <CardGroup className='container  mb-5'>
                         <Card className='m-2'>
-                            <CardImg top width='100%' src={taps} alt='beer taps' />
-                            <CardBody>
-                                <CardTitle className='text-center'>Cicerone Quiz</CardTitle>
+                            <CardImg top width='100%' src={pouring} alt='pouring pairing' />
+                            <CardBody className='d-flex flex-column'>
+                                <CardTitle className='text-center'>Cicerone Manual</CardTitle>
                                 <CardSubtitle>Level 2</CardSubtitle>
-                                <CardText>Test your knowledge and take the Cicerone Quiz</CardText>
-                                <Button outline color='info'><Link to='content'>Start</Link></Button>
+                                <CardText>Gain the knowledge to pass the Certified Cicerone Exam</CardText>
+                                <Button className='mt-auto' outline color='info'><Link to='content'>Study</Link></Button>
                             </CardBody>
                         </Card>
                         <Card className='m-2'>
                             <CardImg top width='100%' src={tasting} alt='tasting beer' />
-                            <CardBody>
+                            <CardBody className='d-flex flex-column'>
                                 <CardTitle className='text-center'>Cicerone Flashcards</CardTitle>
                                 <CardSubtitle>Level 2</CardSubtitle>
                                 <CardText>Prep and Review with Cicerone Flashcards</CardText>
-                                <Button outline color='info'><Link to='content'>Review</Link></Button>
+                                <Button className='mt-auto' outline color='info'><Link to='content'>Review</Link></Button>
                             </CardBody>
                         </Card>
                         <Card className='m-2'>
-                            <CardImg top width='100%' src={pouring} alt='pouring pairing' />
-                            <CardBody>
-                                <CardTitle className='text-center'>Cicerone Manual</CardTitle>
+                            <CardImg top width='100%' src={taps} alt='beer taps' />
+                            <CardBody className='d-flex flex-column'>
+                                <CardTitle className='text-center'>Cicerone Quiz</CardTitle>
                                 <CardSubtitle>Level 2</CardSubtitle>
-                                <CardText>Gain the knowledge to pass the Certified Cicerone Exam</CardText>
-                                <Button outline color='info'><Link to='content'>Study</Link></Button>
+                                <CardText>Test your knowledge and take the Cicerone Quiz</CardText>
+                                <Button className='mt-auto' outline color='info'><Link to='content'>Start</Link></Button>
                             </CardBody>
                         </Card>
                     </CardGroup>
                 </TabPanel>
                 <TabPanel>
-                    <h3>Advanced Cicerone</h3>
-                    <p>Level 3</p>
+                    <div className='text-center mt-4'>
+                        <h3>Advanced Cicerone</h3>
+                        <p>Level 3</p>
+                    </div>
+                    <div>
+                        <h5>Overview:</h5>
+                        <ul>
+                            <li>Those who achieve this certification possess expert knowledge of beer and excellent tasting ability.</li>
+                            <li>Well-prepared candidates will have excellent and extensive style knowledge, hands-on experience managing beer service, direct familiarity with brewing process and ingredients, and frequent and repeated experience in beer and food pairing.</li>
+                        </ul>
+                        <h5>Prerequisites</h5>
+                        <ul>
+                            <li>Passed the Certified Beer Server and Cicerone Exam</li>
+                            <li>Legal drinking age</li>
+                        </ul>
+                    </div>
                     <div className='text-center'>0% Completed</div>
                     <Progress animated striped color='info' value={0} />
                     <CardGroup className='container  mb-5'>
                         <Card className='m-2'>
-                            <CardImg top width='100%' src={pairing} alt='pairing beer' />
-                            <CardBody>
-                                <CardTitle className='text-center'>Advanced Cicerone Quiz</CardTitle>
+                            <CardImg top width='100%' src={tastingTable} alt='beer tasting' />
+                            <CardBody className='d-flex flex-column'>
+                                <CardTitle className='text-center'>Advanced Cicerone Manual</CardTitle>
                                 <CardSubtitle>Level 3</CardSubtitle>
-                                <CardText>Test your knowledge and take the Advanced Cicerone Quiz</CardText>
-                                <Button outline color='info'><Link to='content'>Start</Link></Button>
+                                <CardText>Gain the knowledge to pass the Advanced Cicerone Exam</CardText>
+                                <Button className='mt-auto' outline color='info'><Link to='content'>Study</Link></Button>
                             </CardBody>
                         </Card>
                         <Card className='m-2'>
                             <CardImg top width='100%' src={beer} alt='glases of beer' />
-                            <CardBody>
+                            <CardBody className='d-flex flex-column'>
                                 <CardTitle className='text-center'>Advanced Cicerone Flashcards</CardTitle>
                                 <CardSubtitle>Level 3</CardSubtitle>
                                 <CardText>Prep and Review with Advanced Cicerone Flashcards</CardText>
-                                <Button outline color='info'><Link to='content'>Review</Link></Button>
+                                <Button className='mt-auto' outline color='info'><Link to='content'>Review</Link></Button>
                             </CardBody>
                         </Card>
                         <Card className='m-2'>
-                            <CardImg top width='100%' src={tastingTable} alt='beer tasting' />
-                            <CardBody>
-                                <CardTitle className='text-center'>Advanced Cicerone Manual</CardTitle>
+                            <CardImg top width='100%' src={pairing} alt='pairing beer' />
+                            <CardBody className='d-flex flex-column'>
+                                <CardTitle className='text-center'>Advanced Cicerone Quiz</CardTitle>
                                 <CardSubtitle>Level 3</CardSubtitle>
-                                <CardText>Gain the knowledge to pass the Advanced Cicerone Exam</CardText>
-                                <Button outline color='info'><Link to='content'>Study</Link></Button>
+                                <CardText>Test your knowledge and take the Advanced Cicerone Quiz</CardText>
+                                <Button className='mt-auto' outline color='info'><Link to='content'>Start</Link></Button>
                             </CardBody>
                         </Card>
                     </CardGroup>
                 </TabPanel>
                 <TabPanel>
-                    <h3>Master Cicerone</h3>
-                    <p>Level 4</p>
+                    <div className='text-center mt-4'>
+                        <h3>Master Cicerone</h3>
+                        <p>Level 4</p>
+                    </div>
+                    <div>
+                        <h5>Overview:</h5>
+                        <ul>
+                            <li>In-depth theoretical and hands-on knowledge of draft systems is required, with a special emphasis on troubleshooting and draft system design</li>
+                            <li>Mastery of beer styles is expected with detailed knowledge of the current BJCP guidelines and familiarity with other style systems as well</li>
+                            <li>Tasting experience with a wide range of beers and styles is a must. Candidates should be familiar with a catalog of commercial examples for each style and have the ability to describe their flavors</li>
+                        </ul>
+                        <h5>Prerequisites</h5>
+                        <ul>
+                            <li>Passed the Certified Beer Server, Cicerone, and Advanced Cicerone Exam</li>
+                            <li>Legal drinking age</li>
+                        </ul>
+                    </div>
                     <div className='text-center'>0% Completed</div>
                     <Progress animated striped color='info' value={0} />
                     <CardGroup className='container  mb-5'>
                         <Card className='m-2'>
-                            <CardImg top width='100%' src={pairing} alt='pairing beer' />
-                            <CardBody>
-                                <CardTitle className='text-center'>Master Cicerone Quiz</CardTitle>
+                            <CardImg top width='100%' src={barley} alt='beer tasting' />
+                            <CardBody className='d-flex flex-column'>
+                                <CardTitle className='text-center'>Master Cicerone Manual</CardTitle>
                                 <CardSubtitle>Level 4</CardSubtitle>
-                                <CardText>Test your knowledge and take the Master Cicerone Quiz</CardText>
-                                <Button outline color='info'><Link to='content'>Start</Link></Button>
+                                <CardText>Gain the knowledge to pass the Master Cicerone Exam</CardText>
+                                <Button className='mt-auto' outline color='info'><Link to='content'>Study</Link></Button>
                             </CardBody>
                         </Card>
                         <Card className='m-2'>
                             <CardImg top width='100%' src={hops} alt='serving beer' />
-                            <CardBody>
+                            <CardBody className='d-flex flex-column'>
                                 <CardTitle className='text-center'>Master Cicerone Flashcards</CardTitle>
                                 <CardSubtitle>Level 4</CardSubtitle>
                                 <CardText>Prep and Review with Master Cicerone Flashcards</CardText>
-                                <Button outline color='info'><Link to='content'>Review</Link></Button>
+                                <Button className='mt-auto' outline color='info'><Link to='content'>Review</Link></Button>
                             </CardBody>
                         </Card>
                         <Card className='m-2'>
-                            <CardImg top width='100%' src={barley} alt='beer tasting' />
-                            <CardBody>
-                                <CardTitle className='text-center'>Master Cicerone Manual</CardTitle>
+                            <CardImg top width='100%' src={pairing} alt='pairing beer' />
+                            <CardBody className='d-flex flex-column'>
+                                <CardTitle className='text-center'>Master Cicerone Quiz</CardTitle>
                                 <CardSubtitle>Level 4</CardSubtitle>
-                                <CardText>Gain the knowledge to pass the Master Cicerone Exam</CardText>
-                                <Button outline color='info'><Link to='content'>Study</Link></Button>
+                                <CardText>Test your knowledge and take the Master Cicerone Quiz</CardText>
+                                <Button className='mt-auto' outline color='info'><Link to='content'>Start</Link></Button>
                             </CardBody>
                         </Card>
                     </CardGroup>
